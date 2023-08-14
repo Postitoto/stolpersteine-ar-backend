d0705d307a40670c83627213ad2625f56acdd3ca
ge97rod <ge97rod@mytum.de>
2023-08-14 17:32:06 +0200

    ready for merge

--- stolpersteine/settings.py
+++ stolpersteine/settings.py
@@ -115,6 +115,8 @@ WSGI_APPLICATION = 'stolpersteine.wsgi.application'
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'stolpersteine_ar_database',
         'USER': 'stolpersteine_ar_database_user',
